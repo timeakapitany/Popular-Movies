e815a11e9d2c3a55112562ae9f58de74649f069a
Timea Kapitany <kapitanytimea@gmail.com>
2018-12-15 12:29:23 +0100

    json parser error handling

--- app/src/main/java/com/timeakapitany/popularmovies/JsonParser.java
+++ app/src/main/java/com/timeakapitany/popularmovies/JsonParser.java
@@ -1,8 +1,7 @@
 package com.timeakapitany.popularmovies;
 
-import android.util.Log;
-
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -11,10 +10,11 @@ import java.util.List;
 class JsonParser {
     private static final String TAG = "JsonParser";
 
-    public static List<Movie> parseMovieJson(String json) {
+    static List<Movie> parseMovieJson(String json) {
         List<Movie> movieList = new ArrayList<>();
 
         try {
+
             JSONObject movieObject = new JSONObject(json);
             JSONArray results = movieObject.getJSONArray("results");
 
@@ -22,27 +22,34 @@ class JsonParser {
             for (int i = 0; i < results.length(); i++) {
                 movie = new Movie();
                 JSONObject jsonObject = results.getJSONObject(i);
-                movie.setId(jsonObject.getInt("id"));
-                movie.setOverview(jsonObject.getString("overview"));
-                movie.setPopularity(jsonObject.getDouble("popularity"));
-                movie.setPosterPath(jsonObject.getString("poster_path"));
-                movie.setReleaseDate(jsonObject.getString("release_date"));
-                movie.setTitle(jsonObject.getString("original_title"));
-                movie.setVoteAverage(jsonObject.getDouble("vote_average"));
+                if (jsonObject.has("id")) {
+                    movie.setId(jsonObject.getInt("id"));
+                }
+                if (jsonObject.has("overview")) {
+                    movie.setOverview(jsonObject.getString("overview"));
+                }
+                if (jsonObject.has("popularity")) {
+                    movie.setPopularity(jsonObject.getDouble("popularity"));
+                }
+                if (jsonObject.has("poster_path")) {
+                    movie.setPosterPath(jsonObject.getString("poster_path"));
+                }
+                if (jsonObject.has("release_date")) {
+                    movie.setReleaseDate(jsonObject.getString("release_date"));
+                }
+                if (jsonObject.has("original_title")) {
+                    movie.setTitle(jsonObject.getString("original_title"));
+                }
+                if (jsonObject.has("vote_average")) {
+                    movie.setVoteAverage(jsonObject.getDouble("vote_average"));
+                }
 
                 movieList.add(movie);
             }
 
-
-        } catch (Exception e) {
-            Log.d(TAG, "parseMovieJson: " + e.getMessage());
-
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-
         return movieList;
-
-
     }
-
-
 }
